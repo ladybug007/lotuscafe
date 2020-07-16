78d090003548b05c462037049d0e531c1e9e9ef9
Priya <49821685+ladybug007@users.noreply.github.com>
2020-07-16 18:40:00 -0500

    Updates

---
+++ precache-manifest.c0e872446080a47425f95b6af29f55b0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747b769399128a194b90cb9c5ea56ef9",
+    "url": "/lotuscafe/index.html"
+  },
+  {
+    "revision": "222d3bd4d8c2cfc208c1",
+    "url": "/lotuscafe/static/css/2.f5609993.chunk.css"
+  },
+  {
+    "revision": "8726ef09c9f70ee35ff0",
+    "url": "/lotuscafe/static/css/main.aee9b63b.chunk.css"
+  },
+  {
+    "revision": "222d3bd4d8c2cfc208c1",
+    "url": "/lotuscafe/static/js/2.e3f8b3a0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/lotuscafe/static/js/2.e3f8b3a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8726ef09c9f70ee35ff0",
+    "url": "/lotuscafe/static/js/main.b50c98d7.chunk.js"
+  },
+  {
+    "revision": "612b3858bf6ffdeb596a",
+    "url": "/lotuscafe/static/js/runtime-main.e6cec7b4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/lotuscafe/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/lotuscafe/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/lotuscafe/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/lotuscafe/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/lotuscafe/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/lotuscafe/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/lotuscafe/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/lotuscafe/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/lotuscafe/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/lotuscafe/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
